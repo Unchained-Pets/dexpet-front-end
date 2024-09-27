0d17d6db05638f6813ed7a3c926fb1e38776d737
Akanimorex <akanimoime2016@gmail.com>
2024-09-27 01:55:58 +0100

    Page Hero Updated

--- app/components/PageHero.tsx
+++ app/components/PageHero.tsx
@@ -10,9 +10,9 @@ interface PageHeroProps {
 
 const PageHero:React.FC<PageHeroProps> = ({pageTitle, prev, prevUrl, currentPage}) => {
   return (
-    <div className='flex items-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500 h-[350px] w-screen'>
-      <main className='h-[100px] px-10 xl:w-[1460px] space-y-10'>
-        <span className='text-4xl font-bold text-blue-950 px-6 rounded-l-xl bg-white py-4'>{pageTitle}</span>
+    <div className='flex items-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500 h-[220px] md:h-[350px] w-screen'>
+      <main className='h-[100px] pt-6 md:pt-0 px-5 md:px-10 w-full lg:w-[1260px] xl:w-[1400px] space-y-5 md:space-y-10'>
+        <span className='text-2xl md:text-4xl font-bold text-blue-950 px-6 rounded-l-xl bg-white py-4'>{pageTitle}</span>
         <BreadCrumbs prev={prev} prevUrl={prevUrl} currentPage={currentPage} />
       </main>
     </div>
@@ -20,3 +20,4 @@ const PageHero:React.FC<PageHeroProps> = ({pageTitle, prev, prevUrl, currentPage
 }
 
 export default PageHero
+ 
